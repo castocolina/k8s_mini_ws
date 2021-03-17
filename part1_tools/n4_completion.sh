0b26f928ab4357d3db34f6674eb4a6e12f7e7b98
Casto Colina <castocolina@gmail.com>
2021-03-17 14:19:32 -0300

    Tools doc

--- part1_tools/n4_completion.sh
+++ part1_tools/n4_completion.sh
@@ -3,10 +3,19 @@
 https://minikube.sigs.k8s.io/docs/commands/completion/
 https://kubernetes.io/docs/tasks/tools/install-kubectl/#enabling-shell-autocompletion
 
-# zsh minikube kubectl
+################################
+##           ZSH               #
+################################
+
+# zsh completion minikube/kubectl
 autoload -U +X compinit && compinit
 source <(minikube completion zsh)
 source <(kubectl completion zsh) 
+
+
+################################
+##           BASH              #
+################################
 
 ### Install bash-completion
 # Bash Ubuntu +
@@ -19,6 +28,6 @@ sudo pacman -S bash-completion
 ## Load bash_completion
 source /usr/share/bash-completion/bash_completion
 
-# Install
-source <(minikube completion zsh)
-source <(kubectl completion zsh) 
+# bash completion minikube/kubectl
+source <(minikube completion bash)
+source <(kubectl completion bash) 
