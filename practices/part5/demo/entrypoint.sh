00f31ff3e7263ec10e1e844207faa24cf220c96a
Casto Colina <castocolina@gmail.com>
2021-03-21 17:15:41 -0300

    Setup pvc for logs storage

--- practices/part5/demo/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-pwd; 
-echo ${LOGS_FOLDER}; 
-curl https://raw.githubusercontent.com/castocolina/k8s_mini_ws/main/practices/part5/demo/index.js; 
-ls -la .; 
-while true; 
-do 
-    echo \"$(date)\"; 
-    sleep 1;
-done
